ef20abd37f950953d9d989eb54923d82703609a8
Ishant Arora <ishantarora311@gmail.com>
2025-07-30 11:38:10 +0530

    Initial Project upload

---
+++ BEE/index.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = express();
+var port = 80;
+app.use(express.static('frontend'));
+app.listen(port , function(){
+    console.log('Server is Runiing on port ' + port);
+});
